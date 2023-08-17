8cc140a95e1a61a1fcbf60f9e39ba2cb6ff3bf4f
innfi <innfi.world@gmail.com>
2023-08-17 21:31:36 +0900

    test-fiber: routing, grouping, param and body

--- test-fiber/main.go
+++ test-fiber/main.go
@@ -8,12 +8,39 @@ import (
 	"github.com/gofiber/fiber/v2/middleware/logger"
 )
 
+type TestUser struct {
+	Name  string
+	Email string
+}
+
 func main() {
 	app := fiber.New()
 	app.Use(cors.New())
 
 	app.Get("/", func(c *fiber.Ctx) error {
 		return c.SendString("hi")
+	})
+
+	userApi := app.Group("/user")
+	userApi.Get("/first", func(c *fiber.Ctx) error {
+		dummyResponse := TestUser{
+			Name:  "innfi",
+			Email: "innfi@test.com",
+		}
+
+		return c.JSON(dummyResponse)
+	})
+	userApi.Post("/second/:id", func(c *fiber.Ctx) error {
+		log.Printf("id: %s\n", c.Params("id"))
+
+		payload := new(TestUser)
+		if err := c.BodyParser(payload); err != nil {
+			log.Printf("parse error: %s\n", err)
+		}
+
+		log.Printf("name: %s, email: %s\n", payload.Name, payload.Email)
+
+		return c.SendStatus(200)
 	})
 
 	app.Use(logger.New(logger.Config{
