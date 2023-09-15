2bd893f45d666bb1f1da1da698501d171f3d529d
innfi <innfi.world@gmail.com>
2023-09-15 21:41:52 +0900

    test-swagger: how are people manage to import local docs?

---
+++ test-swagger/main.go
@@ -0,0 +1,33 @@
+package main
+
+import (
+	"log"
+
+	"github.com/gofiber/fiber/v2"
+	fiberSwagger "github.com/swaggo/fiber-swagger"
+)
+
+// @title Swagger Example API
+// @version 1.0
+// @description This is a sample server Petstore server.
+// @termsOfService http://swagger.io/terms/
+
+// @contact.name API Support
+// @contact.url http://www.swagger.io/support
+// @contact.email support@swagger.io
+
+// @license.name Apache 2.0
+// @license.url http://www.apache.org/licenses/LICENSE-2.0.html
+
+// @host petstore.swagger.io
+// @BasePath /v2
+func main() {
+	app := fiber.New()
+
+	app.Get("/swagger/*", fiberSwagger.WrapHandler)
+
+	err := app.Listen(":3000")
+	if err != nil {
+		log.Fatalf("fiber.Listen failed %s", err)
+	}
+}
