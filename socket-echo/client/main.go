df5276d2d0897a7215a65d375ecba9c21b75ec2d
innfi <innfi.world@gmail.com>
2024-12-16 22:24:16 +0900

    socket-echo: let the server handle multiple connections

--- socket-echo/client/main.go
+++ socket-echo/client/main.go
@@ -4,46 +4,123 @@ import (
 	"bufio"
 	"fmt"
 	"net"
-	"os"
+	"time"
+
+	"github.com/google/uuid"
 )
 
 func main() {
-	conn, err := net.Dial("tcp", "localhost:6379")
+	id := uuid.New()
+
+	conn, err := net.Dial("tcp", "localhost:8080")
 	if err != nil {
 		fmt.Println(err)
 		return
 	}
-
 	defer conn.Close()
 
-	reader := bufio.NewReader(os.Stdin)
 	go handleResponse(conn)
 
+	index := 0
 	for {
-		stdinBuf, readErr := reader.ReadString('\n')
-		if readErr != nil {
-			fmt.Printf("err: %s\n", readErr)
-			break
-		}
-		if len(stdinBuf) <= 2 {
-			break
-		}
+		input := fmt.Sprintf("%s] message %d\n", id.String(), index)
+		conn.Write([]byte(input))
 
-		conn.Write([]byte(stdinBuf))
+		index += 1
+
+		time.Sleep(1 * time.Second)
 	}
 }
 
 func handleResponse(conn net.Conn) {
 	fmt.Printf("handleResponse] started\n")
-	respBuf := make([]byte, 100)
+
+	reader := bufio.NewReader(conn)
 
 	for {
-		_, respErr := conn.Read(respBuf)
-		if respErr != nil {
-			fmt.Printf("respErr: %s\n", respErr)
+		message, err := reader.ReadString('\n')
+		if err != nil {
 			break
 		}
 
-		fmt.Printf("handleResponse] message from the server: %s\n", string(respBuf))
+		fmt.Printf("message from the server: %s\n", message)
 	}
 }
+
+// func main() {
+// 	conn, err := net.Dial("tcp", "localhost:9092")
+// 	if err != nil {
+// 		fmt.Println(err)
+// 		return
+// 	}
+//
+// 	defer conn.Close()
+//
+// 	reader := bufio.NewReader(os.Stdin)
+// 	go handleResponse(conn)
+//
+// 	binInput := toByteArray()
+//
+// 	for {
+// 		stdinBuf, readErr := reader.ReadString('\n')
+// 		if readErr != nil {
+// 			fmt.Printf("err: %s\n", readErr)
+// 			break
+// 		}
+// 		if len(stdinBuf) <= 2 {
+// 			break
+// 		}
+//
+// 		conn.Write(binInput)
+//
+// 		time.Sleep(5 * time.Millisecond)
+// 		conn.Write(binInput)
+//
+// 		time.Sleep(5 * time.Millisecond)
+// 		conn.Write(binInput)
+// 	}
+// }
+//
+// func handleResponse(conn net.Conn) {
+// 	fmt.Printf("handleResponse] started\n")
+// 	respBuf := make([]byte, 100)
+//
+// 	for {
+// 		readLen, respErr := conn.Read(respBuf)
+// 		if respErr != nil {
+// 			fmt.Printf("respErr: %s\n", respErr)
+// 			break
+// 		}
+//
+// 		// fmt.Printf("handleResponse] message from the server: %s\n", string(respBuf))
+// 		fmt.Printf("response: \n")
+// 		for index, elem := range respBuf {
+// 			if index >= readLen {
+// 				break
+// 			}
+//
+// 			fmt.Printf("%02x ", elem)
+// 		}
+// 		fmt.Printf("\n")
+// 	}
+// }
+//
+// func toByteArray() []byte {
+// 	binInput := []byte{
+// 		0x00, 0x00, 0x00, 0x23,
+// 		0x00, 0x12,
+// 		// 0x67, 0x4a,
+// 		0x00, 0x04,
+// 		0x4f, 0x74, 0xd2, 0x8b,
+// 		0x00, 0x09,
+// 		0x6b, 0x61, 0x66, 0x6b, 0x61, 0x2d, 0x63, 0x6c, 0x69,
+// 		0x00,
+// 		0x0a,
+// 		0x6b, 0x61, 0x66, 0x6b, 0x61, 0x2d, 0x63, 0x6c, 0x69,
+// 		0x04,
+// 		0x30, 0x2e, 0x31, 0x00,
+// 	}
+//
+// 	return binInput
+// }
+//
--- socket-echo/server/main.go
+++ socket-echo/server/main.go
@@ -1,37 +1,77 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
 	"net"
 )
 
+type Context struct {
+	conn net.Conn
+	ch   chan []byte
+}
+
+var (
+	contexts   = make(map[*Context]bool)
+	register   = make(chan *Context)
+	unregister = make(chan *Context)
+)
+
 func main() {
-	ln, err := net.Listen("tcp", ":8080")
+	listener, err := net.Listen("tcp", ":8080")
 	if err != nil {
 		fmt.Println(err)
 		return
 	}
+	defer listener.Close()
+
+	go handleMessage()
 
 	for {
-		conn, err := ln.Accept()
+		conn, err := listener.Accept()
 		if err != nil {
-			fmt.Println(err)
+			fmt.Println("Error accepting connection:", err.Error())
 			continue
 		}
 
-		go handleConnection(conn)
+		context := &Context{conn: conn, ch: make(chan []byte)}
+		register <- context
+		go handleContext(context)
 	}
 }
 
-func handleConnection(conn net.Conn) {
+func handleMessage() {
 	for {
-		buf := make([]byte, 100)
-		_, err := conn.Read(buf)
+		select {
+		case context := <-register:
+			fmt.Println("received register by channel")
+			contexts[context] = true
+		case context := <-unregister:
+			fmt.Println("received unregister by channel")
+			if _, ok := contexts[context]; ok {
+				delete(contexts, context)
+				close(context.ch)
+			}
+		}
+	}
+}
+
+func handleContext(context *Context) {
+	defer func() {
+		unregister <- context
+		context.conn.Close()
+	}()
+
+	reader := bufio.NewReader(context.conn)
+
+	for {
+		message, err := reader.ReadString('\n')
 		if err != nil {
-			fmt.Println(err)
-			continue
+			break
 		}
 
-		fmt.Printf("message: %s\n", buf)
+		fmt.Printf("message: %s\n", message)
+
+		context.conn.Write([]byte(message))
 	}
 }
